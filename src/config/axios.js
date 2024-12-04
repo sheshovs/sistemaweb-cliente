43a8d12d3f95ad8153661519bfe6b8440538f27d
Sergio Vargas Silva <42360650+sheshovs@users.noreply.github.com>
2024-12-04 18:38:25 -0300

    Update axios.js

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const clienteAxios = axios.create({
-    baseURL: process.env.REACT_APP_BACKEND_URL
+    baseURL: process.env.REACT_APP_BACKEND_URL,
+    withCredentials: true,
 });
 
 export default clienteAxios;
